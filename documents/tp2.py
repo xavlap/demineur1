6dbbbce98d085942ba9b5e1ce1e10cfdce9b1f6d
xavlap <xlapalme@gmial.com>
2020-12-20 11:34:17 -0500

    Update tp2.py

--- documents/tp2.py
+++ documents/tp2.py
@@ -4,8 +4,8 @@
 
 
 #tableau des cartes 
-tabCartes = ['<img src="cards/AC.svg">', '<img src="cards/AD.svg">',
-                    '<img src="cards/AH.svg">', '<img src="cards/AS.svg">',
+tabCartes = [' ', ' ',             #les as vide pour pas les affichers
+                    ' ', ' ',
                     
                     '<img src="cards/2C.svg">', '<img src="cards/2D.svg">',
                     '<img src="cards/2H.svg">', '<img src="cards/2S.svg">',
@@ -72,57 +72,75 @@ def init():
        #jeu table td { border: 0; padding: 1px 2px; height: auto; }
        #jeu table td img { height: auto; }
     ''' 
-    main.innerHTML+= '<div id="jeu">'+afficherCartes(cartesMelanger())+'</div>'
+    ordreDesCartes=melangerTab(list(range(52)))
     
-    #je sais pas pourquoi je peux pas faire ca come si case 1 existe pas mais 
-    #j'écrit bien son id dans td()
-    case1 = document.querySelector("case1").setAttribute("style","backgroud-color: lime")
-    print(case1)
-    #case1.setAttribute("style", "background-color: lime")
+    main.innerHTML+= '<div id="jeu">'+afficherCartes(ordreDesCartes)+'</div>'
     
+    mettreEnVert(ordreDesCartes)
     
 
 def onClic(pos):
     pass
 
-#retourne un packet de 52 cartes melange
-def cartesMelanger():
-    cartes = list(range(52))
-    for i in range(51,0,-1):
+#melange un tableau 
+def melangerTab(tab):
+    for i in range(len(tab)-1,0,-1):
         rand = math.floor(random()*i)
-        temp = cartes[i]
-        cartes[i] = cartes[rand]
-        cartes[rand] = temp
-    return cartes
+        temp = tab[i]
+        tab[i] = tab[rand]
+        tab[rand] = temp
+    return tab
     
+    
+def tdEtIdPourCarte(tabCartes):
+    for i in range(len(tabCartes)):
+        tabCartes[i] = '<td id="case'+str(i)+'">' + tabCartes[i] + '</td>'
+    return tabCartes
+
 def table(contenu): return '<table>' + contenu + '</table>'
 def tr(contenu): return '<tr>' + contenu + '</tr>'
 
-#mauvais nom de fonction
-def td(contenu,case):
-    print('<td id="case'+str(case)+'">' + contenu + '</td>')
-    return '<td id="case1">' + contenu + '</td>' #test pour etre sur d'avoir id="case1"
-    #return '<td id="case"'+str(case)+'">' + contenu + '</td>' #se qu'il faudra mettre
-
-
 def afficherCartes(cartes):
     global tabCartes
+    cartesPasMelange = tdEtIdPourCarte(tabCartes.copy())
     tabCartesMelange =['']*52
     
     for i in range(52):
-        if cartes[i]==0 or cartes[i]==1 or cartes[i]==2 or cartes[i]==3:
-            continue        
-        tabCartesMelange[i] = tabCartes[cartes[i]]
+        tabCartesMelange[i] = cartesPasMelange[cartes[i]]
     
-        
-    string = (tr(''.join(map(td,tabCartesMelange[0:13],list(range(13))))))
-    string += (tr(''.join(map(td,tabCartesMelange[13:26],list(range(13,26))))))
-    string += (tr(''.join(map(td,tabCartesMelange[26:39],list(range(26,39))))))
-    string += (tr(''.join(map(td,tabCartesMelange[39:52],list(range(39,52))))))
+    string = (tr(''.join(tabCartesMelange[0:13])))
+    string += (tr(''.join(tabCartesMelange[13:26])))
+    string += (tr(''.join(tabCartesMelange[26:39])))
+    string += (tr(''.join(tabCartesMelange[39:52])))
     
     return table(string)
     
+
+def mettreEnVert(cartes):
+    global tabCartes
     
+    positionVide0 = cartes.index(0)-1
+    positionVide1 = cartes.index(1)-1
+    positionVide2 = cartes.index(2)-1
+    positionVide3 = cartes.index(3)-1
+    print(tabCartes[cartes[positionVide0]])
+    print(tabCartes[cartes[positionVide1]])
+    print(tabCartes[cartes[positionVide2]])
+    print(tabCartes[cartes[positionVide3]])
+    
+    for i in range(52):
+        if i%4 == cartes[positionVide0]%4 and i//4 == cartes[positionVide0]//4:
+            document.querySelector("#case" + str(i+4)).setAttribute("style", "background-color: lime")
+        if i%4 == cartes[positionVide1]%4 and i//4 == cartes[positionVide1]//4:
+            document.querySelector("#case" + str(i+4)).setAttribute("style", "background-color: lime")
+        if i%4 == cartes[positionVide2]%4 and i//4 == cartes[positionVide2]//4:
+            document.querySelector("#case" + str(i+4)).setAttribute("style", "background-color: lime")
+        if i%4 == cartes[positionVide3]%4 and i//4 == cartes[positionVide3]//4:
+            document.querySelector("#case" + str(i+4)).setAttribute("style", "background-color: lime")
+    print(positionVide0)
+    print(positionVide1)
+    print(positionVide2)
+    print(positionVide3)
     
     
     
